c51c816b737405127fb9298fef719d19ace241f6
Ashley U <ashley.umetsu@gmail.com>
2021-12-07 10:26:22 -0600

    Displaying and clearing chores

--- chore-bot/src/List.js
+++ chore-bot/src/List.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
-const List = () => {
-    return (
-        <>
-            <h2>List of Chores</h2>
-        </>
-    );
+const List = ({ chores }) => {
+  return (
+    <>
+      {chores.map((chore) => {
+        const { id, name, day, status, image } = chore;
+        return (
+          <article key={id} className='chore'>
+            <img src={image} />
+            <div>
+              <h4>{name}</h4>
+              <p>{day}</p>
+              <p>{status}</p>
+            </div>
+          </article>
+        );
+      })}
+    </>
+  );
 };
+
+export default List;

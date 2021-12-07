b48a38496f049819542c467d9b04873fa16d0b91
DamonCrutchfield <crutchfielddamond@gmail.com>
2021-12-07 15:15:51 -0600

    create function that removes chore from list when status button is clicked

--- chore-bot/src/List.js
+++ chore-bot/src/List.js
@@ -1,19 +1,23 @@
 import React from 'react';
 
 const List = ({ chores }) => {
+  const resolveChore = (id) => {
+    let elem = document.getElementById(id);
+    elem.remove();
+  }
   return (
     <>
       {chores.map((chore) => {
         const { id, name, day, status, image } = chore;
         return (
-          <article key={id} className='chore'>
+          <article key={id} id={id} className='chore'>
             <img src={image} className="chore-images" />
             <div>
                 <h4>{name}</h4>
                 <p>{day}</p>
                 <p>{status}</p>
-                <img src="../icons/delete-button.png" onClick={() => console.log("Clicked")} className="remove-button"/>   
-                <img src="../icons/check.png" onClick={() => console.log("Clicked")} className="complete-button"/>
+                <img src="../icons/delete-button.png" onClick={() => resolveChore(id)} className="remove-button"/>   
+                <img src="../icons/check.png" onClick={() => resolveChore(id)} className="complete-button"/>
             </div>
           </article>
         );

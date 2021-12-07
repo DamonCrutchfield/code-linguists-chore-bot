973fec5a5a831d54a00755cdabe204267c0b145e
Ashley U <ashley.umetsu@gmail.com>
2021-12-07 09:46:17 -0600

    Default styling

---
+++ chore-bot/src/data.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        id: 1,
+        name: 'Clean the bathrooms',
+        day: 'Sunday',
+        status: true
+    },
+    {
+        id: 2,
+        name: 'Sweep',
+        day: 'Saturday',
+        status: true
+    },
+    {
+        id: 3,
+        name: 'Dust',
+        day: 'Tuesday',
+        status: true
+    },
+    {
+        id: 4,
+        name: 'Mop',
+        day: 'Saturday',
+        status: true
+    },
+    {
+        id: 5,
+        name: 'Take out trash',
+        day: 'Monday',
+        status: true
+    }
+];

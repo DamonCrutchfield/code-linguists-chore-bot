ddd2361179a6a523e2f4acc203dcde1b6459987e
Chad Eckles <chad1020@gmail.com>
2021-12-07 18:15:25 -0600

    data fix

--- chore-bot/src/data.js
+++ chore-bot/src/data.js
@@ -6,5 +6,45 @@ export default [
         status: true,
         image: '../icons/clean-dishes.png',
         due:"17:25:00:2021-12-7"
-    }
+    },
+    {
+        id: 2,
+        name: 'Sweep',
+        day: 'Saturday',
+        status: true,
+        image: '../icons/sweep.png',
+        due:"12/7/2021 @ 16:48"
+    },
+    {
+        id: 3,
+        name: 'Dust',
+        day: 'Tuesday',
+        status: true,
+        image: '../icons/dust.png',
+        due:"12/7/2021 @ 16:"
+    },
+    {
+        id: 4,
+        name: 'Mop',
+        day: 'Saturday',
+        status: true,
+        image: '../icons/mop.png',
+        due:"12/7/2021 @ 19:50"
+    },
+    {
+        id: 5,
+        name: 'Take out trash',
+        day: 'Monday',
+        status: true,
+        image: '../icons/trash.png',
+        due:"12/7/2021 @ 16:50"
+    },
+    {
+        id: 5,
+        name: 'Laundry',
+        day: 'Wednesday',
+        status: true,
+        image: '../icons/washing.png',
+        due:"12/7/2021 @ 16:50"
+    },
 ];

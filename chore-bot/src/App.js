e90f77104b440fc110f7552ff1a5e64426097feb
Chad Eckles <chad1020@gmail.com>
2021-12-07 18:14:47 -0600

    working loop function

--- chore-bot/src/App.js
+++ chore-bot/src/App.js
@@ -15,22 +15,26 @@ function App() {
     }
     //timer to check chore for popup reminder.
     async function checkChoreLoop() {
-        const currentdate = new Date(); 
-        const datetime = currentdate.getDate() + "/"
-                    + (currentdate.getMonth()+1)  + "/" 
-                    + currentdate.getFullYear() + " @ "  
-                    + currentdate.getHours() + ":"  
-                    + currentdate.getMinutes() + ":" 
-                    + currentdate.getSeconds();
-        console.log(datetime);
-        toast("Clean your House");
+        let today = new Date();
+        let time = today.getHours() + ":" + today.getMinutes() + ":" + "00";
+        let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+
+        for(const chore in data){
+            //console.log(data[chore].due + ":"+ datetime);
+            console.log(time +":"+date);
+            const match = false;
+            if((time+":"+date) === data[chore].due){
+                console.log("match");
+                toast(data[chore].name);
+            }else{console.log("no match")}
+        }
     } 
-    const MINUTE_MS = 30000;
+    const MINUTE_MS = 10000;
     useEffect(() => {
         const interval = setInterval(() => {
         //TODO: add function to loop throught chore and toast when time for chore.
-          toast("Clean your House!");
-          //checkChoreLoop();
+          //toast("Clean your House!");
+          checkChoreLoop();
         }, MINUTE_MS);
       
         return () => clearInterval(interval); 
--- chore-bot/src/data.js
+++ chore-bot/src/data.js
@@ -4,41 +4,7 @@ export default [
         name: 'Wash Dishes',
         day: 'Sunday',
         status: true,
-        image: '../icons/clean-dishes.png'
-    },
-    {
-        id: 2,
-        name: 'Sweep',
-        day: 'Saturday',
-        status: true,
-        image: '../icons/sweep.png'
-    },
-    {
-        id: 3,
-        name: 'Dust',
-        day: 'Tuesday',
-        status: true,
-        image: '../icons/dust.png'
-    },
-    {
-        id: 4,
-        name: 'Mop',
-        day: 'Saturday',
-        status: true,
-        image: '../icons/mop.png'
-    },
-    {
-        id: 5,
-        name: 'Take out trash',
-        day: 'Monday',
-        status: true,
-        image: '../icons/trash.png'
-    },
-    {
-        id: 5,
-        name: 'Laundry',
-        day: 'Wednesday',
-        status: true,
-        image: '../icons/washing.png'
-    },
+        image: '../icons/clean-dishes.png',
+        due:"17:25:00:2021-12-7"
+    }
 ];
